7d1ccf83d0657edfa701a7342b3f1b8f2a98b4db
Zhaoyilunnn <dilved1014@gmail.com>
2023-04-15 07:46:10 +0000

    research: update find paper tool

---
+++ research/README.md
@@ -0,0 +1,5 @@
+# Search papers in DBLP
+
+```
+./find_papers.sh <year> <conference> <search-key>
+```
--- research/find_papers.sh
+++ research/find_papers.sh
@@ -21,26 +21,40 @@ function search_all() {
 }
 
 function search_specific() {
-    echo "Search specific conferences ..."
-    for y in $(seq ${year}); do
-        local link="${dblp_src}${conf}/${conf}${y}.html"
-        curl ${link} 1>html.${conf}.${y} 2>/dev/null
+    local year_start=$1
+    local year_end=$2
+    local conf_dir=$3
+    local conf_key=$4
+    local key=$5
+
+    echo "Search ${key} in conference ${conf_dir}, "\
+        "from year ${year_start} to year ${year_end} ..."
+
+    for y in $(seq ${year_start} ${year_end}); do
+        local link="${dblp_src}${conf_dir}/${conf_key}${y}.html"
+        curl ${link} 1>html.${conf_dir}.${y} 2>/dev/null
+        python3 html_parser.py html.${conf_dir}.${y} | grep -i ${key} | grep "^Data"
     done
-    for y in $(seq ${year}); do
-        python3 html_parser.py html.${conf}.${y} | grep -i ${key} | grep "^Data"
-    done
-    rm -rf html.${conf}.*
+    rm -rf html.${conf_dir}.*
 }
 
 if [ $# -eq 3 ]; then
-    year=$(echo $1 | awk -F'-' '{if(NF==2){print $1,$2} else {print $1}}')
-    conf=$2
+    year_args=$(echo $1 | awk -F'-' '{if(NF==2){print $1,$2} else {print $1,$1}}')
+    #year=()
+    #for k in $year_args; do
+    #    year+=($k)
+    #done
+    conf_args=$(echo $2 | awk -F'-' '{if(NF==2){print $1,$2} else {print $1,$1}}')
+    #conf=()
+    #for k in $conf_args; do
+    #    conf+=($k)
+    #done
     key=$3
-    mode=1
+    search_specific $year_args $conf_args $key
 elif [ $# -eq 2 ]; then
     year=$1
     key=$2
-    mode=0
+    search_all
 else
     echo "Usage: $0 <year> <key> ::Search all conferences in specific year"
     echo "       $0 <year> <conf> <key> ::Search specific conference in specific year"
@@ -48,9 +62,4 @@ else
     exit 1
 fi
 
-if [ ${mode} -eq 0 ]; then
-    search_all
-elif [ ${mode} -eq 1 ]; then
-    search_specific
-fi
 
