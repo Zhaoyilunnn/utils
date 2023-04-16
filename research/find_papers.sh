93dfb897411cd922828a143f54b9034066df911c
Zhaoyilunnn <dilved1014@gmail.com>
2023-04-16 15:12:57 +0000

    research: update

--- research/find_papers.sh
+++ research/find_papers.sh
@@ -21,17 +21,27 @@ function search_all() {
 }
 
 function search_specific() {
-    local year_start=$1
-    local year_end=$2
-    local conf_dir=$3
-    local conf_key=$4
-    local key=$5
+    if [ $# -eq 5 ]; then
+        local year_start=$1
+        local year_end=$2
+        local conf_dir=$3
+        local conf_key=$4
+        local key=$5
+    elif [ $# -eq 6 ]; then
+        local year_start=$1
+        local year_end=$2
+        local suffix=-$3
+        local conf_dir=$4
+        local conf_key=$5
+        local key=$6
+    fi
+
     local conf_name=$(echo "${conf_dir}" | tr '[:lower:]' '[:upper:]')
 
     echo "Search ${key} in conference ${conf_dir}, from year ${year_start} to year ${year_end} ..."
 
     for y in $(seq ${year_start} ${year_end}); do
-        local link="${dblp_src}${conf_dir}/${conf_key}${y}.html"
+        local link="${dblp_src}${conf_dir}/${conf_key}${y}${suffix}.html"
         curl ${link} 1>html.${conf_dir}.${y} 2>/dev/null
         python3 html_parser.py html.${conf_dir}.${y} |
             grep -i ${key} | grep "^Data" |
@@ -41,7 +51,15 @@ function search_specific() {
 }
 
 if [ $# -eq 3 ]; then
-    year_args=$(echo $1 | awk -F'-' '{if(NF==2){print $1,$2} else {print $1,$1}}')
+    year_args=$(echo $1 | awk -F'-' '{
+        if(NF>1) {
+            for (i=1; i<=NF; i++) {
+                printf "%s ", $i
+            }
+        } else {
+            print $1,$1
+        }
+    }')
     conf_args=$(echo $2 | awk -F'-' '{if(NF==2){print $1,$2} else {print $1,$1}}')
     key=$3
     search_specific $year_args $conf_args $key
