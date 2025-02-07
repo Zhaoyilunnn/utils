70bbdff64f7c7ce51ef94af8af89f9322067d3f5
zhao-yilun-zyl <dilved1014@gmail.com>
2025-02-07 14:19:49 +0800

    feat(find_paper): support journals

--- research/find_papers.sh
+++ research/find_papers.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 dblp_src="https://dblp.org/db/conf/"
+dblp_src_journal="https://dblp.org/db/journals/"
 conference_list="dac date isca micro aspdac iccad hpca asplos"
+
+# only limited journals are supported
+journal_list="tcad tc"
 mode=0 # 0: search all conferences, 1: search specific conferences
 year=2021
 key="Light"
@@ -30,7 +34,7 @@ function search_specific() {
     elif [ $# -eq 6 ]; then
         local year_start=$1
         local year_end=$2
-        local suffix=-$3
+        local suffix="-$3"
         local conf_dir=$4
         local conf_key=$5
         local key=$6
@@ -38,7 +42,7 @@ function search_specific() {
 
     local conf_name=$(echo "${conf_dir}" | tr '[:lower:]' '[:upper:]')
 
-    echo "Search ${key} in conference ${conf_dir}, from year ${year_start} to year ${year_end} ..."
+    echo "Searching \"${key}\" in conference ${conf_dir}, from year ${year_start} to year ${year_end} ..."
 
     for y in $(seq ${year_start} ${year_end}); do
         local link="${dblp_src}${conf_dir}/${conf_key}${y}${suffix}.html"
@@ -48,6 +52,27 @@ function search_specific() {
             awk '{ gsub(/^[^:]*:/, "", $0); print "-"$0" '"${conf_name}"'-'"${y}"'"}'
     done
     rm -rf html.${conf_dir}.*
+}
+
+function search_journal() {
+    local vol_start=$1
+    local vol_end=$2
+    local journal_dir=$3
+    local journal_name=$4
+    local key=$5
+
+    local journal_upper=$(echo "${journal_name}" | tr '[:lower:]' '[:upper:]')
+
+    echo "Searching \"${key}\" in journal ${journal_name}, from volume ${vol_start} to ${vol_end} ..."
+
+    for v in $(seq ${vol_start} ${vol_end}); do
+        local link="${dblp_src_journal}${journal_dir}/${journal_name}${v}.html"
+        curl ${link} 1>html.${journal_name}.${v} 2>/dev/null
+        python3 html_parser.py html.${journal_name}.${v} |
+            grep -i ${key} | grep "^Data" |
+            awk '{ gsub(/^[^:]*:/, "", $0); print "-"$0" '"${journal_upper}"'-Volume-'"${v}"'"}'
+    done
+    rm -rf html.${journal_name}.*
 }
 
 if [ $# -eq 3 ]; then
@@ -60,9 +85,14 @@ if [ $# -eq 3 ]; then
             print $1,$1
         }
     }')
-    conf_args=$(echo $2 | awk -F'-' '{if(NF==2){print $1,$2} else {print $1,$1}}')
     key=$3
-    search_specific $year_args $conf_args $key
+    # check if it is journal by checking if the name is in journal list
+    if [[ " ${journal_list[@]} " =~ " $2 " ]]; then
+        search_journal $year_args $2 $2 $key
+    else
+        conf_args=$(echo $2 | awk -F'-' '{if(NF==2){print $1,$2} else {print $1,$1}}')
+        search_specific $year_args $conf_args $key
+    fi
 elif [ $# -eq 2 ]; then
     year=$1
     key=$2
