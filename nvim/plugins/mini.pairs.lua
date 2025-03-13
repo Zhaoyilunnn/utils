df07fc417bea15a6a26de9d0a5c58d1fe208ce4c
zhao-yilun-zyl <dilved1014@gmail.com>
2025-03-13 23:20:51 +0800

    chore: disable auto pair for '"' in python

--- nvim/plugins/mini.pairs.lua
+++ nvim/plugins/mini.pairs.lua
@@ -1,26 +1,26 @@
 return {
-	"echasnovski/mini.pairs",
-	event = "VeryLazy",
-	opts = {
-		modes = { insert = true, command = true, terminal = false },
-		-- skip autopair when next character is one of these
-		skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
-		-- skip autopair when the cursor is inside these treesitter nodes
-		skip_ts = { "string" },
-		-- skip autopair when next character is closing pair
-		-- and there are more closing pairs than opening pairs
-		skip_unbalanced = true,
-		-- better deal with markdown code blocks
-		markdown = true,
-	},
-	--- test if $HOME changed
-	config = function(_, opts)
-		LazyVim.mini.pairs(opts)
-		vim.api.nvim_create_autocmd("FileType", {
-			pattern = "python",
-			callback = function(args)
-				vim.keymap.del("i", '"', { buffer = args.buf })
-			end,
-		})
-	end,
+  "echasnovski/mini.pairs",
+  event = "VeryLazy",
+  opts = {
+    modes = { insert = true, command = true, terminal = false },
+    -- skip autopair when next character is one of these
+    skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+    -- skip autopair when the cursor is inside these treesitter nodes
+    skip_ts = { "string" },
+    -- skip autopair when next character is closing pair
+    -- and there are more closing pairs than opening pairs
+    skip_unbalanced = true,
+    -- better deal with markdown code blocks
+    markdown = true,
+  },
+  --- test if $HOME changed
+  config = function(_, opts)
+    LazyVim.mini.pairs(opts)
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "python",
+      callback = function(args)
+        vim.keymap.set("i", '"', '"', { buffer = args.buf })
+      end,
+    })
+  end,
 }
