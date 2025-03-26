fb06c0319692d469383346ebc632eda6b4195024
zhao-yilun-zyl <dilved1014@gmail.com>
2025-03-26 18:39:26 +0800

    feat: update

--- dev/nvim/init.lua
+++ dev/nvim/init.lua
@@ -1,5 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.opt.spell = true
-vim.opt.spelllang = "en_us"
---
+++ dev/nvim/lua/plugins/copilotchapt.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+      model = "o3-mini",
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+}
