33e3d944e9eb0b615d72b375b4445f9747aa3ab9
Zhaoyilunnn <dilved1014@gmail.com>
2025-03-30 18:17:15 +0800

    feat: update nvim

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# utils
+# Content
+
+- `dev`:  development related configurations.
+- `gist`: legacy gist related scripts.
+- `figure`: figure related scripts.
+- `research`: research related scripts.
+- `windows`: utility scripts for windows.
--- dev/nvim/lua/config/options.lua
+++ dev/nvim/lua/config/options.lua
@@ -2,6 +2,12 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- spelling check
 vim.opt.spell = true
 vim.opt.spelllang = "en_us"
+
+-- change line automatically
 vim.opt.wrap = true
+
+-- disable AI completion in nvim-cmp / blink.cmp
+vim.g.ai_cmp = false
