d5eb945b481857c88d4e9ed1ebda8c89ed3096c8
zhao-yilun-zyl <dilved1014@gmail.com>
2025-05-26 23:39:37 +0800

    chore: update nvim

--- dev/nvim/lua/plugins/copilotchat.lua
+++ dev/nvim/lua/plugins/copilotchat.lua
@@ -8,7 +8,7 @@ return {
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
       -- See Configuration section for options
-      model = "o3-mini",
+      model = "gemini-2.5-pro",
     },
     -- See Commands section for default commands if you want to lazy load on them
   },
